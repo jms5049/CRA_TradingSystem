0a260ed0826ec66e0fb0242f956da0ab429ed54b
ahran_.choi <carokq@gmail.com>
2024-06-03 16:26:11 +0900

    정리

--- TradingSystemTest/TradingSystem_test.cpp
+++ TradingSystemTest/TradingSystem_test.cpp
@@ -8,7 +8,6 @@ using namespace testing;
 
 class BrokerMock : public StockBroker {
 public:
-	MOCK_METHOD(void, selectBroker, (string brokerName), (override));
 	MOCK_METHOD(void, login, (string id, string pw), (override));
 	MOCK_METHOD(void, buy, (string stockCode, int price, int amount), (override));
 	MOCK_METHOD(void, sell, (string stockCode, int price, int amount), (override));
